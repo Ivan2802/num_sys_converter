185ee9aaaeb9034392572f7db308c5a20839bdc5
Ivan2802 <funny_patrick@mail.ru>
2024-11-26 22:29:00 +0300

    Added comments

--- converter.py
+++ converter.py
@@ -61,3 +61,9 @@ def convert(num, basis_from, basis_to, round_to = 100):
     if basis_to == 10: return convert_to_10(str(num).lower(), basis_from)
     else: return convert_from_10(convert_to_10(str(num).lower(), basis_from), basis_to, round_to)
 
+# This is a function for converting numbers (integers or floating points) to any number system you want to use
+# To use the convert() function:
+# The first argument is the number you want to convert
+# The second argument is which system you are converting the number from
+# The third argument is which system you are converting the number to
+# The fourth argument (optional) is how many characters you want to see after the dot
